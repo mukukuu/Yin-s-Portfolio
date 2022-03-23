22cead15d0ad5da68881e7b0e53af14dc21a4a0f
mukukuu <qinii@me.com>
2022-03-23 13:39:19 -0700

    added helper.js code and imported all components into app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Header from './components/Header';
+import Resume from './components/Resume';
+import Nav from './components/Nav';
+import Content from './components/Content';
+import AboutMe from './components/AboutMe';
+import Project from "./components/Project";
+import ContactForm from './components/Contact';
+import Footer from './components/Footer';
+
 function App() {
+
+
+
+
+
+
+  
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/components/Footer/index.js
@@ -0,0 +1,6 @@
+import React,{ useState } from 'react';
+import './App.css';
+import Nav from './components/Nav';
+import About from './components/About';
+import Gallery from "./components/Gallery";
+import ContactForm from './components/Contact';
